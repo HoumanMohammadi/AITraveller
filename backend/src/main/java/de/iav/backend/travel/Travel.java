66b7252a9288f0f79b2f78c2969fe250c86a881b
Houman Mohammadi <houmane.el@gmail.com>
2023-09-13 11:35:06 +0200

    User Package enhanced

---
+++ backend/src/main/java/de/iav/backend/travel/Travel.java
@@ -0,0 +1,4 @@
+package de.iav.backend.travel;
+
+public class Travel {
+}
--- backend/src/main/java/de/iav/backend/user/UserRepository.java
+++ backend/src/main/java/de/iav/backend/user/UserRepository.java
@@ -1,4 +1,14 @@
 package de.iav.backend.user;
 
-public interface UserRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByEmail(String email);
+
+    Optional<User> findUserByEmail(String userEmail);
+
 }
