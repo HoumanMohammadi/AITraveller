2d7a69f07195746f6f352fc88784255984299044
Houman Mohammadi <houmane.el@gmail.com>
2023-09-13 11:50:19 +0200

    Travel.java created

--- backend/src/main/java/de/iav/backend/travel/Travel.java
+++ backend/src/main/java/de/iav/backend/travel/Travel.java
@@ -1,4 +1,26 @@
 package de.iav.backend.travel;
 
+import de.iav.backend.travelSuggestion.TravelSuggestion;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Travel {
+
+    @MongoId
+    String id;
+    TravelSuggestion travelSuggestion;
+    LocalDateTime localDateTime;
+    int age;
+    List<String> coTraveller;
+    String livingCity;
+    boolean disability;
+    int travelDuration;
+    List<String> activity;
+    String season;
+    List<String> meansOfTravel;
+    List<String> travelPurpose;
+    List<String> destinationContinent;
+
 }
