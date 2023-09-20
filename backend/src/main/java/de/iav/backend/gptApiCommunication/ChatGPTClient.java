4688ba0a9d3a7020d60c419e0196305945b2ff6c
Houman Mohammadi <houmane.el@gmail.com>
2023-09-20 13:51:57 +0200

    api key secured

--- backend/src/main/java/de/iav/backend/gptApiCommunication/ChatGPTClient.java
+++ backend/src/main/java/de/iav/backend/gptApiCommunication/ChatGPTClient.java
@@ -6,14 +6,28 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @Service
 @RequiredArgsConstructor
 public class ChatGPTClient {
     private static final String API_URL = "https://api.openai.com/v1/chat/completions"; // Example API URL
     private final WebClient webClient= WebClient.create(API_URL);
     private final Gson gson;
-
     public APIResponse getChatSuggestion(APIRequest request) {
+
+        Properties properties = new Properties();
+        try (InputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String apiKey = properties.getProperty("api.key");
         // Serialize the request object to JSON
         String jsonRequest = gson.toJson(request);
         System.out.println("request: "+ request.toString());
@@ -21,7 +35,7 @@ public class ChatGPTClient {
 
         // Send the request using WebClient
         APIResponse response= webClient.post()
-                .header("Authorization", "Bearer "+"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") // Replace with your actual API key
+                .header("Authorization", "Bearer "+ apiKey) // Replace with your actual API key
                 .header("Content-Type", "application/json")
                 .body(BodyInserters.fromValue(jsonRequest))
                 .retrieve()
