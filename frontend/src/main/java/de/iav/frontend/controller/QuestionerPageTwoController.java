df04e650dc454774870ec5d64af6d9a168802884
Houman Mohammadi <houmane.el@gmail.com>
2023-09-27 10:02:11 +0200

    controller of questioner page two implemented!

--- frontend/src/main/java/de/iav/frontend/controller/QuestionerPageTwoController.java
+++ frontend/src/main/java/de/iav/frontend/controller/QuestionerPageTwoController.java
@@ -12,65 +12,46 @@ import java.io.IOException;
 public class QuestionerPageTwoController {
 
     private final SceneSwitchService sceneSwitchService= SceneSwitchService.getInstance();
-
+    @FXML
+    public RadioButton springRadioButton;
+    @FXML
+    public RadioButton summerRadioButton;
+    @FXML
+    public RadioButton fallRadioButton;
+    @FXML
+    public RadioButton winterRadioButton;
     @FXML
     private ChoiceBox<String> travelDurationChoiceBox;
-
     @FXML
     private CheckBox hikingCheckbox;
-
     @FXML
     private CheckBox scubaDivingCheckbox;
-
     @FXML
     private CheckBox surfingCheckbox;
-
     @FXML
     private CheckBox yogaCheckbox;
-
     @FXML
     private CheckBox skiingCheckbox;
-
     @FXML
     private CheckBox swimmingCheckbox;
-
     @FXML
     private CheckBox sailingCheckbox;
-
     @FXML
     private CheckBox pokerCheckbox;
-
     @FXML
     private CheckBox sunBathingCheckbox;
-
     @FXML
     private ToggleGroup seasonToggleGroup;
-
-    @FXML
-    private Button nextButton;
-
-    @FXML
-    private Button backButton;
     @FXML
     private User user;
-
     @FXML
     private void initialize() {
         if (travelDurationChoiceBox != null) {
             travelDurationChoiceBox.getItems().addAll("1 day", "2 days", "3 days", "4 days", "5 days");
             travelDurationChoiceBox.setValue("1 day");
         }
-    }
 
-
-    @FXML
-    private void handleNextButtonAction() {
-        // Code to handle Next button click
-    }
-
-    @FXML
-    private void handleBackButtonAction() {
-        // Code to handle Back button click
+        seasonToggleGroup.getToggles().addAll(springRadioButton, summerRadioButton, fallRadioButton, winterRadioButton);
     }
 
     @FXML
@@ -83,16 +64,23 @@ public class QuestionerPageTwoController {
             System.out.println("No season selected.");
         }
     }
-
     public void setUserForQuestioner(User user) {
     }
 
     public void switchToPageOne(ActionEvent event) throws IOException {
         sceneSwitchService.switchToQuestionerPageOne(event, user);
     }
-
-    // Add methods for handling checkbox selections if needed
-
-    // Add any other methods you need
-
+    @FXML
+    private void handleCheckboxAction() {
+        // Example: Check if the hiking checkbox is selected
+        boolean isHikingSelected = hikingCheckbox.isSelected();
+        boolean isScubaDivingSelected= scubaDivingCheckbox.isSelected();
+        boolean isSurfingSelected= surfingCheckbox.isSelected();
+        boolean isYogaSelected= yogaCheckbox.isSelected();
+        boolean isSkiingSelected= skiingCheckbox.isSelected();
+        boolean isSwimmingSelected= swimmingCheckbox.isSelected();
+        boolean isSailingSelected= sailingCheckbox.isSelected();
+        boolean isPokerSelected= pokerCheckbox.isSelected();
+        boolean isSunBathingSelected= sunBathingCheckbox.isSelected();
+    }
 }
